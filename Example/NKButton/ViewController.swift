1902e44a4c12b2fce24c836d269c33e19b368521
Nam Kennic <namkennic@me.com>
2020-04-26 10:15:09 +0700

    no message

--- Example/NKButton/ViewController.swift
+++ Example/NKButton/ViewController.swift
@@ -117,7 +117,11 @@ class ViewController: UIViewController {
 		let allButtons = [loginButton, facebookButton, twitterButton, forgotButton, flashButton]
 		allButtons.forEach { (button) in
 			button.addTarget(self, action: #selector(onButtonSelected), for: .touchUpInside)
-			button.enableHoverGesture()
+			if #available(iOSApplicationExtension 13.0, *) {
+				button.enableHoverGesture()
+			} else {
+				// Fallback on earlier versions
+			}
 			button.backgroundColors[.hovered] = .red
 			if #available(iOS 13.4, *) {
 				button.enablePointerInteraction()
