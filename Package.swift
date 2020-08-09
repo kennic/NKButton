6b70d5cdfac580b5f0b1cb55fc042d80e991ab0c
Nam Kennic <namkennic@me.com>
2020-08-09 20:11:53 +0700

    no message

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NKButton",
-	platforms: [.iOS(.v8)],
+	platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "NKButton",
